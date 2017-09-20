f2969e019bb5115ee5d1475b78411c976f5b39ba
wayne-zhou <997549962@qq.com>
2017-09-20 16:46:43 +0800

    update folder route

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #hexo 生成静态文件
 hexo generate
 #复制public文件夹下文件到关联库的本地地址
-cp -R public/* .deploy/wayne-zhou.github.io/
-cd .deploy/wayne-zhou.github.io/
+cp -R public/* .deploy_git/
+cd .deploy_git/
 git add .
 git commit -m “update”
 git push origin master
